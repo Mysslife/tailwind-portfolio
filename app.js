5f7e38b8b37a11a6c1b8977352a21eee514ab21d
Mysslife <ndqthai97@gmail.com>
2022-11-29 23:37:35 +0700

    final commit

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const hamburger = document.querySelector("#hamburger");
 const menu = document.querySelector("#menu");
+const links = document.querySelectorAll('#hLink');
+const body = document.querySelector('body');
+const moonBtn = document.querySelector("#moon")
 
 hamburger.addEventListener("click", () => {
   menu.classList.toggle("hidden");
   hamburger.classList.toggle("bg-white");
 });
+
+links.forEach(link => link.addEventListener('click', () => {
+  menu.classList.toggle("hidden");
+  hamburger.classList.toggle("bg-white");
+}))
+
+moonBtn.addEventListener('click', () => {
+  body.classList.toggle("dark");
+})
